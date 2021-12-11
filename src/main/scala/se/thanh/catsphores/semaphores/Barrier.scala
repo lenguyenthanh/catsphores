a4401be8e35b803625707ed9e0b7cf57eb5cf9ee
Thanh Le <lenguyenthanh@hotmail.com>
2021-12-11 12:55:00 +0100

    Add barrier

---
+++ src/main/scala/se/thanh/catsphores/semaphores/Barrier.scala
@@ -0,0 +1,74 @@
+package se.thanh.catsphores.semaphores
+
+import scala.concurrent.duration.*
+
+import cats.effect.kernel.Deferred
+import cats.effect.std.{Console, Semaphore}
+import cats.effect.syntax.all.*
+import cats.effect.{ExitCode, IO, IOApp, Temporal}
+import cats.implicits.*
+
+/** Consider again the Rendezvous problem. A limitation of the solution we presented is that it does
+  * not work with more than two threads. Generalize the rendezvous solution. Every thread should run
+  * the following code:
+  *
+  * rendezvous critical point
+  *
+  * The synchronization requirement is that no thread executes critical point until after all
+  * threads have executed rendezvous.
+  *
+  * Our solution is also called preloaded turnstile which allows all threads run concurrently
+  */
+
+object Barrier extends IOApp {
+  def run(args: List[String]): IO[ExitCode] = program(10L).as(ExitCode.Success)
+
+  def program(total: Long): IO[Unit] =
+    for {
+      mutex <- Deferred[IO, Unit]
+      signal <- Semaphore[IO](0)
+      barrier = Barrier(total, mutex, signal)
+      tasks = List.range(0L, total).map(n => BTask(n, mutex, signal).use())
+      allTasks = barrier.use() :: tasks
+      _ <- allTasks.parSequence.void
+    } yield ()
+
+}
+
+class BTask[F[_]: Temporal](
+  number: Long,
+  barrier: Deferred[F, Unit],
+  signal: Semaphore[F],
+)(
+  implicit F: Console[F]
+) {
+
+  def use(): F[Unit] =
+    for {
+      _ <- F.println(s"Task $number started")
+      _ <- signal.release
+      y <- signal.available
+      _ <- F.println(s"Availability: $y")
+      _ <- barrier.get
+      _ <- F.println(s"Task $number after critical point")
+    } yield ()
+
+}
+
+class Barrier[F[_]: Temporal](
+  tasks: Long,
+  mutex: Deferred[F, Unit],
+  signal: Semaphore[F],
+)(
+  implicit F: Console[F]
+) {
+
+  def use(): F[Unit] =
+    for {
+      _ <- F.println("Barrier started")
+      _ <- signal.acquireN(tasks)
+      _ <- mutex.complete(())
+      _ <- F.println("Barrier completed")
+    } yield ()
+
+}
--- src/main/scala/se/thanh/catsphores/semaphores/Multiplex.scala
+++ src/main/scala/se/thanh/catsphores/semaphores/Multiplex.scala
@@ -6,9 +6,6 @@ import cats.effect.std.{Console, Semaphore}
 import cats.effect.syntax.all.*
 import cats.effect.{ExitCode, IO, IOApp, Temporal}
 import cats.implicits.*
-import cats.instances.int
-
-import se.thanh.catsphores.Debug.*
 
 /** Generalize the previous solution so that it allows multiple threads to run in the critical
   * section at the same time, but it enforces an upper limit on the number of concurrent threads. In
@@ -17,9 +14,20 @@ import se.thanh.catsphores.Debug.*
   */
 object Multiplex extends IOApp {
 
-  var count = 0
-
   def run(args: List[String]): IO[ExitCode] = program.as(ExitCode.Success)
+
+  val program: IO[Unit] =
+    for {
+      s <- Semaphore[IO](2)
+      r1 = new PreciousResource[IO]("R1", s)
+      r2 = new PreciousResource[IO]("R2", s)
+      r3 = new PreciousResource[IO]("R3", s)
+      r4 = new PreciousResource[IO]("R4", s)
+      r5 = new PreciousResource[IO]("R5", s)
+      r6 = new PreciousResource[IO]("R6", s)
+      _ <- List(r1.use, r2.use, r3.use, r4.use, r5.use, r6.use).parSequence.void
+    } yield ()
+
 }
 
 class PreciousResource[F[_]: Temporal](name: String, s: Semaphore[F])(implicit F: Console[F]) {
@@ -37,15 +45,3 @@ class PreciousResource[F[_]: Temporal](name: String, s: Semaphore[F])(implicit F
     } yield ()
 
 }
-
-val program: IO[Unit] =
-  for {
-    s <- Semaphore[IO](2)
-    r1 = new PreciousResource[IO]("R1", s)
-    r2 = new PreciousResource[IO]("R2", s)
-    r3 = new PreciousResource[IO]("R3", s)
-    r4 = new PreciousResource[IO]("R4", s)
-    r5 = new PreciousResource[IO]("R5", s)
-    r6 = new PreciousResource[IO]("R6", s)
-    _ <- List(r1.use, r2.use, r3.use, r4.use, r5.use, r6.use).parSequence.void
-  } yield ()
