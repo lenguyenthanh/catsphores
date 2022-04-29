7071ad45943bb3e54b290fa967d6ab35b03536c2
Thanh Le <lenguyenthanh@hotmail.com>
2022-04-29 10:12:05 +0200

    Bump deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Cats {
-    val catsEffect = "org.typelevel" %% "cats-effect" % "3.3.9"
+    val catsEffect = "org.typelevel" %% "cats-effect" % "3.3.11"
     val catsMtl = "org.typelevel" %% "cats-mtl" % "1.2.1"
 
     val all = Seq(catsEffect, catsMtl)
@@ -12,7 +12,7 @@ object Dependencies {
   object Tests {
     val munit = "org.scalameta" %% "munit" % "0.7.29" % Test
     val munitCatsEffect = "org.typelevel" %% "munit-cats-effect-3" % "1.0.7" % Test
-    val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.15.4" % Test
+    val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.16.0" % Test
     val munitScalaCheck = "org.scalameta" %% "munit-scalacheck" % "0.7.29" % Test
 
     val all = Seq(munit, munitScalaCheck, munitCatsEffect, scalaCheck)
